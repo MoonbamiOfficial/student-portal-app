276db81f06d850e9bf233082c90bd33512bf849f
sublimemac <makipactaoin@gmail.com>
2023-11-09 20:02:46 +0700

    feat: add ui to login form

--- src/StudentPortal/LoginForm.cs
+++ src/StudentPortal/LoginForm.cs
@@ -16,5 +16,10 @@ namespace StudentPortal
         {
             InitializeComponent();
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
