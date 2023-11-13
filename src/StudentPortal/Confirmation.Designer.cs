56aa48258277c7e233c201cdb82238d58d410657
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-13 14:54:00 +0800

    feat: add confimation box

---
+++ src/StudentPortal/Confirmation.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace StudentPortal
+{
+    partial class Confirmation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HeaderLabel = new System.Windows.Forms.Label();
+            this.HeaderTwoLabel = new System.Windows.Forms.Label();
+            this.ReminderLabel = new System.Windows.Forms.Label();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.ConfirmBtn = new System.Windows.Forms.Button();
+            this.BottomBorder = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // HeaderLabel
+            // 
+            this.HeaderLabel.BackColor = System.Drawing.Color.Transparent;
+            this.HeaderLabel.Font = new System.Drawing.Font("Poppins SemiBold", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.HeaderLabel.Location = new System.Drawing.Point(260, 139);
+            this.HeaderLabel.Name = "HeaderLabel";
+            this.HeaderLabel.Size = new System.Drawing.Size(759, 154);
+            this.HeaderLabel.TabIndex = 0;
+            this.HeaderLabel.Text = "Are you sure about the\ndetails you’ve entered? ";
+            this.HeaderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HeaderTwoLabel
+            // 
+            this.HeaderTwoLabel.BackColor = System.Drawing.Color.Transparent;
+            this.HeaderTwoLabel.Font = new System.Drawing.Font("Poppins Medium", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderTwoLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.HeaderTwoLabel.Location = new System.Drawing.Point(327, 323);
+            this.HeaderTwoLabel.Name = "HeaderTwoLabel";
+            this.HeaderTwoLabel.Size = new System.Drawing.Size(626, 90);
+            this.HeaderTwoLabel.TabIndex = 1;
+            this.HeaderTwoLabel.Text = "Some information cannot be changed \nonce you hit confirm";
+            this.HeaderTwoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ReminderLabel
+            // 
+            this.ReminderLabel.BackColor = System.Drawing.Color.Transparent;
+            this.ReminderLabel.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReminderLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.ReminderLabel.Location = new System.Drawing.Point(361, 553);
+            this.ReminderLabel.Name = "ReminderLabel";
+            this.ReminderLabel.Size = new System.Drawing.Size(558, 28);
+            this.ReminderLabel.TabIndex = 2;
+            this.ReminderLabel.Text = "After signing up, student number will be auto generated";
+            this.ReminderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(86)))), ((int)(((byte)(63)))));
+            this.CancelBtn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.CancelBtn.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelBtn.ForeColor = System.Drawing.Color.White;
+            this.CancelBtn.Location = new System.Drawing.Point(665, 443);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(300, 80);
+            this.CancelBtn.TabIndex = 3;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = false;
+            // 
+            // ConfirmBtn
+            // 
+            this.ConfirmBtn.BackColor = System.Drawing.Color.Transparent;
+            this.ConfirmBtn.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(86)))), ((int)(((byte)(63)))));
+            this.ConfirmBtn.FlatAppearance.BorderSize = 3;
+            this.ConfirmBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ConfirmBtn.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConfirmBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(86)))), ((int)(((byte)(63)))));
+            this.ConfirmBtn.Location = new System.Drawing.Point(334, 443);
+            this.ConfirmBtn.Name = "ConfirmBtn";
+            this.ConfirmBtn.Size = new System.Drawing.Size(300, 80);
+            this.ConfirmBtn.TabIndex = 4;
+            this.ConfirmBtn.Text = "Confirm";
+            this.ConfirmBtn.UseVisualStyleBackColor = false;
+            // 
+            // BottomBorder
+            // 
+            this.BottomBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(159)))), ((int)(((byte)(127)))));
+            this.BottomBorder.Location = new System.Drawing.Point(0, 620);
+            this.BottomBorder.Name = "BottomBorder";
+            this.BottomBorder.Size = new System.Drawing.Size(1280, 100);
+            this.BottomBorder.TabIndex = 5;
+            // 
+            // Confirmation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(245)))), ((int)(((byte)(229)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.HeaderLabel);
+            this.Controls.Add(this.HeaderTwoLabel);
+            this.Controls.Add(this.ReminderLabel);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.ConfirmBtn);
+            this.Controls.Add(this.BottomBorder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Confirmation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Confirmation";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HeaderLabel;
+        private System.Windows.Forms.Label HeaderTwoLabel;
+        private System.Windows.Forms.Label ReminderLabel;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Button ConfirmBtn;
+        private System.Windows.Forms.Panel BottomBorder;
+    }
+}
