d2c164ede375e389e417d1da5469d64728410407
Jireh-sama <jirehtumbagahan@gmail.com>
2023-11-13 18:29:53 +0800

    feat: create student class

---
+++ src/StudentPortal/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPortal
+{
+    internal class Student
+    {
+        static string firstName = "";
+        static string middleName = "";
+        static string lastName = "";
+        
+    }
+}
