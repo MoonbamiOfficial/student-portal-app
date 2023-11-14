231abdb3c06dee7f95130c0dda6c38af95b8900b
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-11-14 13:45:20 +0800

    feat: add custom picture box

---
+++ src/StudentPortal/StudentPortalControls/pictureBoxCustom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace StudentPortal.StudentPortalControls
+{
+    internal class pictureBoxCustom : PictureBox
+    {
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            GraphicsPath graphPath = new GraphicsPath();
+            graphPath.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(graphPath);
+            base.OnPaint(pe);
+        }
+    }
+}
