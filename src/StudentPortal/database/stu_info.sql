bb15542108ed112a341f831b562fe047a18f9a45
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2023-12-04 22:19:37 +0800

    feat: add database

---
+++ src/StudentPortal/database/stu_info.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2023 at 03:17 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stu_portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stu_info`
+--
+
+CREATE TABLE `stu_info` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(155) NOT NULL,
+  `email` varchar(155) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `sex` varchar(50) NOT NULL,
+  `birthday` varchar(150) NOT NULL,
+  `course` varchar(200) NOT NULL,
+  `yearLevel` int(20) NOT NULL,
+  `semester` int(20) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `stu_number` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stu_info`
+--
+ALTER TABLE `stu_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stu_info`
+--
+ALTER TABLE `stu_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
